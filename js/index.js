ae1d791bee3c870519f38ec4782655aae8ea8758
Angello Ordonez <angelo-mjz7@hotmail.com>
2021-02-27 12:55:45 -0500

    ejemplo

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
 const btn = document.querySelectorAll('button')
-let i = 1;
-
 const notify = new Notify();
-const notify2 = new Notify();
-btn[0].addEventListener("click", _=>{
-    notify.success('Se ha registrado el usuario: '+i, 'Todo ok');
-    i++;
-})
 
+btn[0].addEventListener("click", _=>{
+    notify.infor('Notify está trabajando correctamente');
+})
 btn[1].addEventListener("click", _=>{
-    notify.warning('Se ha registrado el usuario: '+i, 'Todo ok');
-    i++;
+    notify.success('Se registro el usuario correctamente', 'Registro Exitoso');
 })
 btn[2].addEventListener("click", _=>{
-    notify.error('Se ha registrado el usuario: '+i, 'Todo ok');
-    i++;
+    notify.warning('El correo ingresado ya ha sido registrado', 'Alerta');
 })
 btn[3].addEventListener("click", _=>{
-    notify.infor('Se ha registrado el usuario: '+i, 'Todo ok');
-    i++;
+    notify.error('No se pudo registrar el usuario', 'Error de registro');
 })
+
+const notify2 = new Notify();
 btn[4].addEventListener("click", _=>{
     notify2.options.activeCustomIcon = true;
     notify2.options.customIcon = '<i class="fas fa-envelope"></i>';
-    notify2.infor('Este es un icono personalizado ', 'Todo ok');
+    notify2.infor('Ud. tiene 20 e-mail sin leer', 'Email');
 })
 
